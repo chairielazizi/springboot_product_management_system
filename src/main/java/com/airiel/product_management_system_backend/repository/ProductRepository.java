4f7e8d262f1956ed7fccc0084af24683151965f7
chairielazizi <azizihr2000@gmail.com>
2025-02-16 13:35:57 +0800

    restructure project

--- src/main/java/com/airiel/product_management_system_backend/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.airiel.product_management_system_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.airiel.product_management_system_backend.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
