ef5f0ea8d004fba147c81cf52d084e7733223cf8
chairielazizi <azizihr2000@gmail.com>
2025-03-24 07:10:42 +0800

    Added CORS configuration to WebConfig class

--- product_management_system_backend/src/main/java/com/airiel/product_management_system_backend/config/WebConfig.java
+++ product_management_system_backend/src/main/java/com/airiel/product_management_system_backend/config/WebConfig.java
@@ -1,9 +1,19 @@
 package com.airiel.product_management_system_backend.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://product-management-system-backend-rrvu.onrender.com")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
